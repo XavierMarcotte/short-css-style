9102971734544cd2fc6105e8a76282d969ccf8fa
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-04-08 14:29:27 +0200

    petite animation que j'ai vu sur tiktok mais mieux structuree et avec du javascript pour ne pas reecrire tout les nthchild manuellement

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const ul = document.querySelector('ul');
 const number = ul.children.length;
  console.log("🚀 ~ file: index.js:10 ~ number:", number)
 
-
-
-
 for (i=1, j=0.2; i<=number; i+=1, j+=0.2) {
     const element = document.createElement('style');
     element.textContent = `ul li:nth-child(${i}) {
